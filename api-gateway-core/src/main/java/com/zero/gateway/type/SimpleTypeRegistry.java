4cd6e4d7dea949fd10973385e634903da1b49c57
zero <1315436708@qq.com>
2023-08-05 17:46:46 +0800

    HTTP请求参数解析,包括GET/POST，以及应对不同Content-Type类型的处理

---
+++ api-gateway-core/src/main/java/com/zero/gateway/type/SimpleTypeRegistry.java
@@ -0,0 +1,49 @@
+package com.zero.gateway.type;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @description: 基本类型注册器
+ * @author: ZeroYiAn
+ * @time: 2023/8/5
+ */
+
+public class SimpleTypeRegistry {
+
+    private static final Set<String> SIMPLE_TYPE_SET = new HashSet<>();
+
+    static {
+        SIMPLE_TYPE_SET.add(String.class.getName());
+        SIMPLE_TYPE_SET.add(Byte.class.getName());
+        SIMPLE_TYPE_SET.add(Short.class.getName());
+        SIMPLE_TYPE_SET.add(Character.class.getName());
+        SIMPLE_TYPE_SET.add(Integer.class.getName());
+        SIMPLE_TYPE_SET.add(Long.class.getName());
+        SIMPLE_TYPE_SET.add(Float.class.getName());
+        SIMPLE_TYPE_SET.add(Double.class.getName());
+        SIMPLE_TYPE_SET.add(Boolean.class.getName());
+        SIMPLE_TYPE_SET.add(Date.class.getName());
+        SIMPLE_TYPE_SET.add(Class.class.getName());
+        SIMPLE_TYPE_SET.add(BigInteger.class.getName());
+        SIMPLE_TYPE_SET.add(BigDecimal.class.getName());
+    }
+
+    private SimpleTypeRegistry() {
+        // Prevent Instantiation
+    }
+
+    /*
+     * Tells us if the class passed in is a known common type
+     *
+     * @param clazz The class to check
+     * @return True if the class is known
+     */
+    public static boolean isSimpleType(String clazz) {
+        return SIMPLE_TYPE_SET.contains(clazz);
+    }
+
+}
